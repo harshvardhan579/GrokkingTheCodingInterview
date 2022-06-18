61880a2b2e81e2aceab5119bf43816f049cffef9
harshvardhan579 <67804644+harshvardhan579@users.noreply.github.com>
2022-06-18 16:38:19 +0530

    Update maxSumSubArrSizeK.py

--- maxSumSubArrSizeK.py
+++ maxSumSubArrSizeK.py
@@ -1,3 +1,5 @@
+# Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
+
 def max_sub_array_of_size_k(k, arr):
   # TODO: Write your code here
   maxSum, winSum = 0, 0
